b6d709e506a79d7308fe68c9ac822d4ad242adbc
liuzzom <33182647+liuzzom@users.noreply.github.com>
2019-01-17 22:12:22 +0100

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,33 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class Driver extends Configured implements Tool{
+    public int run(String[] args) throws Exception{
+        Configuration conf = getConf();
+
+        Job job = Job.getInstance(conf, "WordCount");
+        job.setJarByClass(Driver.class);
+        job.setMapperClass(Map.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        System.out.println("Input dir: " + "/home/mauroliuzzo/IdeaProjects/Tweets-First-Job/input/");
+        System.out.println("Output dir: " + "/home/mauroliuzzo/IdeaProjects/Tweets-First-Job/output/");
+        FileInputFormat.addInputPath(job, new Path("/home/mauroliuzzo/IdeaProjects/Tweets-First-Job/input/"));
+        FileOutputFormat.setOutputPath(job, new Path("/home/mauroliuzzo/IdeaProjects/Tweets-First-Job/output/"));
+        boolean success = job.waitForCompletion(true);
+        return success ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new Driver(), args);
+        System.exit(res);
+    }
+}
---
+++ Map.java
@@ -0,0 +1,42 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.Text;
+/*import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;*/
+
+
+public class Map extends Mapper<Object, Text, Text, Text>{
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+        try(FileReader in = new FileReader("/home/mauroliuzzo/IdeaProjects/Tweets-First-Job/dataset/keywords.txt"); BufferedReader inFile = new BufferedReader(in)){
+            // Keywords import
+            ArrayList<String> keywords = new ArrayList<>();
+            String str = null;
+            do{
+                str = inFile.readLine();
+                if(str!= null){
+                    keywords.add(str);
+                }
+            }while(str != null);
+
+            // Tweets from input file
+            String tweetfile = value.toString();
+            String [] tweets = tweetfile.split("\\n");
+
+            for(String tweet : tweets){
+                for(int i = 0; i < keywords.size(); i++){
+                    if(tweet.contains(keywords.get(i))){
+                        context.write(new Text(keywords.get(i)), new Text(tweet));
+                    }
+                }
+            }
+
+        }
+    }
+}
